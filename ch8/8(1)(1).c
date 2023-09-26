10a1752593db619b6a225a5a2c9a007cf1903b73
prachi <144036679+prachimanani01@users.noreply.github.com>
2023-09-26 19:43:27 +0530

    Add files via upload

---
+++ ch8/8(1)(1).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+    int a[5]={1,2,3,4,5};                                                                                                                                                                                          
+    int i;
+
+    for(i=0;i<=5;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+
+}
