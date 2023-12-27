92eb3671066fdd82914fa577bf5f61180b47966a
prachi <144036679+prachimanani01@users.noreply.github.com>
2023-12-27 17:30:45 +0530

    Update pr4(7).c

--- PR4/pr4(7).c
+++ PR4/pr4(7).c
@@ -1,15 +1,26 @@
 #include<stdio.h>
 
-
 void main(){
-int i;
-
- for(i=1;i<=6;i++){
-  if(i==1||i==3){
-   printf("* * * * *\n");
-  }
-  else if(i==2||i==4||i==5||i==6)
-    printf("*       *\n");
- }
-
+	
+	int i,j,k;
+	for (i=5;i>=1;i--){
+		for(k=i;k>=1;k--){
+			printf(" ");
+		}
+		for(j=5;j>=i;j--){
+			if(j==5){
+				printf("*");
+			}else{
+				printf(" ");
+			}
+		}
+		for(j=i+1;j<=5;j++){
+			if(j==5){
+				printf("*");
+			}else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 }
